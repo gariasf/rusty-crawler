3e3d0a2a9b465316b8ced9ac85d7ba9c4f0fa1cd
Guillem Arias Fauste <hello@gariasf.com>
2024-05-21 12:22:51 +0200

    render entities

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,65 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State{}
+// Plain Old Data
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    foreground: RGB,
+    background: RGB
+}
+
+struct State {
+    ecs: World
+}
+
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
+    fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "rusty crawler!");
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for(pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.foreground, render.background, render.glyph);
+        }
     }
 }
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
-    .with_title("Rusty Crawler")
-    .build()?;
+        .with_title("Rusty Crawler")
+        .build()?;
 
-    let game_state = State{};
-    rltk::main_loop(context, game_state)
+    let mut gamestate = State {
+        ecs: World::new()
+    };
+
+    gamestate.ecs.register::<Position>();
+    gamestate.ecs.register::<Renderable>();
+
+    for i in 0..10 {
+        gamestate.ecs
+            .create_entity()
+            .with(Position {x: i * 7, y: 20})
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                foreground: RGB::named(rltk::RED),
+                background: RGB::named(rltk::BLACK)
+            })
+            .build();
+
+    }
+
+
+
+    rltk::main_loop(context, gamestate)
 }
