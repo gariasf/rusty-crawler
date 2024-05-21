db551bda8433cda96facedd8ef249121d51f38d6
Guillem Arias Fauste <hello@gariasf.com>
2024-05-21 10:58:49 +0200

    add init example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{Rltk, GameState};
+
+struct State{}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "rusty crawler!");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+    .with_title("Rusty Crawler")
+    .build()?;
+
+    let game_state = State{};
+    rltk::main_loop(context, game_state)
 }
