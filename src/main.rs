99b6bdbc0b98f3fcfff86e510e0917f60861ee9c
Guillem Arias Fauste <hello@gariasf.com>
2024-05-21 12:34:49 +0200

    moving entities

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,61 @@
-use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
-use std::cmp::{max, min};
 use specs_derive::Component;
 
 // Plain Old Data
 #[derive(Component)]
 struct Position {
     x: i32,
-    y: i32
+    y: i32,
 }
 
 #[derive(Component)]
 struct Renderable {
     glyph: rltk::FontCharType,
     foreground: RGB,
-    background: RGB
+    background: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
 struct State {
-    ecs: World
+    ecs: World,
 }
 
 impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
+    fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
-        for(pos, render) in (&positions, &renderables).join() {
+        for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.foreground, render.background, render.glyph);
         }
+    }
+}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut left_walker = LeftWalker {};
+        left_walker.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -45,20 +71,20 @@ fn main() -> rltk::BError {
 
     gamestate.ecs.register::<Position>();
     gamestate.ecs.register::<Renderable>();
+    gamestate.ecs.register::<LeftMover>();
 
     for i in 0..10 {
         gamestate.ecs
             .create_entity()
-            .with(Position {x: i * 7, y: 20})
+            .with(Position { x: i * 7, y: 20 })
             .with(Renderable {
                 glyph: rltk::to_cp437('☺'),
                 foreground: RGB::named(rltk::RED),
-                background: RGB::named(rltk::BLACK)
+                background: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
-
     }
-
 
 
     rltk::main_loop(context, gamestate)
